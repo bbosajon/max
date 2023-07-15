694fdb8637f30ed553168cba039320442d3c815c
bbosajon <steinsjb@gmail.com>
2023-07-15 22:04:48 +0300

    debug set for production

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%eckd70i##mznm!a55p^yb&3unlw3!r)d3_g*=1zrx$_*_hy^5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "maxmall.azurewebsites.net",
                  "https://maxmall.azurewebsites.net", "127.0.0.1", "127.0.0.1:8000"]
